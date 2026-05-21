61a7b52c2900c5841b365c220e023ebc447407b1
Maria Matejka <mq@ucw.cz>
2026-05-21 21:09:18 +0200

    Artifact manipulation. Upload doesn't work but no clue why.

--- Runner.py
+++ Runner.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+import io
 import json
+import pathlib
 import requests
 import time
+import zipfile
 import zlib
 #from .Config import Config as C
 
@@ -31,9 +34,62 @@ class Job:
         self.artifacts = artifacts
         self.dependencies = dependencies
 
+        self.upload_artifacts = []
+
         self.kwargs = kwargs
         self._log = []
         self._loglen = 0
+
+    def download_artifacts(self, target):
+        c = self.runner.config
+        if not isinstance(target, pathlib.Path):
+            target = pathlib.Path(target)
+
+        target.mkdir(exist_ok=True)
+
+        for dep in self.dependencies:
+            if "artifacts_file" not in dep:
+                continue
+
+            r = requests.request(
+                    method="GET",
+                    url=c.url + "/api/v4/" + f"jobs/{dep['id']}/artifacts?direct_download=true",
+                    headers={
+                        "Job-Token": self.token,
+                        },
+                    )
+
+            r.raise_for_status()
+            with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+                z.extractall(path=target)
+
+    def checkin_artifact(self, file, apath=None, reldir=None, onsuccess=True, onfailure=True):
+        if reldir is None:
+            reldir = pathlib.Path(".")
+        elif not isinstance(reldir, pathlib.Path):
+            reldir = pathlib.Path(reldir)
+
+        if isinstance(file, pathlib.Path):
+            if apath is None:
+                 apath = file.relative_to(reldir)
+            elif not isinstance(apath, pathlib.Path):
+                apath = pathlib.Path(apath)
+        elif apath is None:
+            raise TypeError(f"File is not pathlib.Path and apath is not given.")
+
+        if not isinstance(onfailure, bool):
+            raise TypeError(f"checkin_artifact(onfailure=...) needs bool, got {onfailure}")
+
+        if not isinstance(onsuccess, bool):
+            raise TypeError(f"checkin_artifact(onsuccess=...) needs bool, got {onsuccess}")
+
+        self.upload_artifacts.append({
+            "file": file,
+            "apath": apath,
+            "failed": onfailure,
+            "success": onfailure,
+            })
+
 
     def update_state(self, state, **kwargs):
         JobState(self, state, **kwargs).exchange()
@@ -47,7 +103,7 @@ class Job:
 
         c = self.runner.config
         r = requests.request(
-                method="PATCH", 
+                method="PATCH",
                 url=c.url + "/api/v4/" + f"jobs/{self.id}/trace?debug_trace=false",
                 headers={
                     "Content-Type": "text/plain",
@@ -65,21 +121,90 @@ class Job:
         self.update_state("running")
         return self
 
-    def __exit__(self, et, *args):
-        if et:
-            self.update_state("failed")
-        else:
-            fulllog = b"".join(self._log)
-            crc = zlib.crc32(fulllog)
+    def __exit__(self, et, ev, tb):
+        c = self.runner.config
 
-            self.update_state(
-                    state="success",
-                    checksum=f"crc32:{crc:08x}",
-                    output={
-                        "checksum": f"crc32:{crc:08x}",
-                        "bytesize": self._loglen,
+        if et:
+            self.log(f"Job failed: et={et}, ev={ev}, tb={tb}")
+            state = "failed"
+        else:
+            self.log("Job successful")
+            state = "success"
+
+        try:
+            ua = 0
+            self.log("Uploading artifacts ...")
+            with zipfile.ZipFile(b := io.BytesIO(), "w") as z:
+                d = { "/": True }
+                for a in self.upload_artifacts:
+                    if a[state]:
+                        if (pp := pathlib.Path(a["apath"]).parent) not in d:
+                            d[pp] = True
+                            dd = [ pp ]
+
+                            while (pp := dd[0].parent) not in d:
+                                d[pp] = True
+                                dd.insert(0, pp)
+
+                            for ddd in dd:
+                                z.mkdir(str(ddd))
+
+                        if isinstance(a["file"], pathlib.Path):
+                            z.write(a["file"], arcname=a["apath"])
+                            self.log(f"-> {a['file']} as {a['apath']}, size {a['file'].stat().st_size}")
+                        else:
+                            z.writestr(a["apath"], a["file"])
+                            self.log(f"-> blob as {a['apath']}, size {len(a['file'])}")
+
+                        ua += 1
+
+            data = b.getvalue()
+
+            boundary = b"OoOoO"
+            while boundary in data:
+                boundary += b"oO"
+
+            fulldata = (
+                        b'--' + boundary + b'\n' +
+                        b'Content-Disposition: form-data; name="file"; filename="artifacts.zip"\n' +
+                        b'Content-Type: application/octet-stream\n' +
+                        data + b'\n' +
+                        b'--' + boundary + b'--')
+            print(f"meow: {fulldata} endmeow")
+
+            r = requests.request(
+                    method='POST',
+                    url=c.url + "/api/v4/" + f"jobs/{self.id}/artifacts?artifact_format=zip&artifact_type=archive",
+                    headers={
+                        "Job-Token": self.token,
+                        "Content-Type": f"multipart/form-data; boundary={boundary.decode()}",
                         },
+                    data=fulldata,
                     )
+
+            if not r.ok:
+                self.log(f"Upload response body: {r.content}")
+
+            r.raise_for_status()
+
+            self.log(f"Uploaded {ua} artifacts.")
+
+        except Exception as e:
+            self.log(f"Failed to upload artifacts: {type(e)}: {e}")
+            state = "failed"
+
+
+        fulllog = b"".join(self._log)
+        crc = zlib.crc32(fulllog)
+
+        self.update_state(
+                state=state,
+                checksum=f"crc32:{crc:08x}",
+                output={
+                    "checksum": f"crc32:{crc:08x}",
+                    "bytesize": self._loglen,
+                    },
+                )
 
 class Runner:
     def cmd(self, cls):
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from Config import Config
 #    token = "glrt-..." (received by registering the runner)
 #    url = "https://..." (the base URI, without /api/v4)
 
+import pathlib
 import time
 
 class TestRunner(Runner):
@@ -20,6 +21,15 @@ r = TestRunner(Config)
 print(r.verify())
 
 for j in r.jobs():
-    for q in range(10):
+    j.download_artifacts("wrooo")
+    for q in range(4):
         j.log(f"meow {q}")
+#        j.checkin_artifact(f"meow {q}", apath=f"meow{q}.txt")
+        with open(f"wrooo/nya{q}.txt", "a") as n:
+            n.write("meow")
+#            j.checkin_artifact(pathlib.Path(f"wrooo/nya{q}.txt"), apath=f"nya/wrooo{q}.txt")
+#            j.checkin_artifact(f"nya {q}", apath=f"a/b/{q}/d/e/f.txt")
+
         time.sleep(1)
+
+    j.checkin_artifact("SAMALAMADINGDONG", apath="changegamer")
